2aef48b8a7e4e271b2ed1cca8266f28f2103aa06
miliberlin <michelle.liebheit@posteo.de>
2020-12-16 16:50:44 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let body = document.getElementById('message-output');
+let txtFld = document.getElementById("text-input");
+let timeStamp = document.createElement("div");
+timeStamp.classList.add("time");
+
+
+function addMessage() {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("message-bubble", "message-robot");
+    // message
+    newDiv.innerHTML = "Hello, how are you?";
+
+    getTime();
+    newDiv.appendChild(timeStamp);
+
+    body.appendChild(newDiv);
+}
+setTimeout(addMessage, 1200);
+
+// send message and clear input
+function sendMessage() {
+    if (txtFld.value != "") {
+        let newMessage = document.createElement("div");
+        newMessage.classList.add("message-bubble", "message-client");
+        newMessage.innerHTML = txtFld.value;
+
+        getTime();
+        newMessage.appendChild(timeStamp);
+
+        body.appendChild(newMessage);
+        txtFld.value = "";
+    }
+}
+
+function getTime() {
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes();
+    timeStamp.innerHTML = time;
+}
