baa156c43e1f6faa25fdc12a317b3c173fd4726b
miliberlin <michelle.liebheit@posteo.de>
2020-12-17 11:17:03 +0100

    added random answer function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,40 @@ let txtFld = document.getElementById("text-input");
 let timeStamp = document.createElement("div");
 timeStamp.classList.add("time");
 
+let answers = [
+    "What is your favourite colour?",
+    "The weather is nice today!",
+    "What is your favourite animal?",
+    "Do you like cookies?",
+    "What did you had for breakfast?",
+    "Every day is a good day!",
+    "What's your favourite planet?",
+    "How would you call a cactus?"
+]
+
+// add time stamp
+function getTime() {
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes();
+    timeStamp.innerHTML = time;
+}
+
+// create random answer
+function createAnswer() {
+    let answerIndex = Math.floor(Math.random()*(answers.length));
+    let answerContent = answers[answerIndex];
+    answers.splice(answerIndex, 1);
+
+    let newAnswer = document.createElement("div");
+    newAnswer.classList.add("message-bubble", "message-robot");
+    newAnswer.innerHTML = answerContent;
+
+    // getTime();
+    // newMessage.appendChild(timeStamp);
+
+    body.appendChild(newAnswer);
+}
+
 // intro message
 function addMessage() {
     let newDiv = document.createElement("div");
@@ -29,12 +63,8 @@ function sendMessage() {
 
         body.appendChild(newMessage);
         txtFld.value = "";
-    }
-}
 
-// add time stamp
-function getTime() {
-    let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes();
-    timeStamp.innerHTML = time;
+        setTimeout(createAnswer, 1200);
+
+    }
 }
